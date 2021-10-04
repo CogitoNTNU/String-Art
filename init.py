6bea35993e1a4036ae7444f36afc88c526383023
Jon Rodtang <jonrodtang@gmail.com>
2021-10-05 01:30:20 +0200

    Remade initializatoin function with numpy

--- init.py
+++ init.py
@@ -1,14 +1,12 @@
-import random
+import numpy as np
 #define number of lines
 #define number of pins
 #generate list of length lines with values 0 to pins-1
 #return initialized list
+#import sys
+#np.set_printoptions(threshold=sys.maxsize)
 
 def initialization(lines: int, pins: int):
-    initList = []
-    for i in range(lines):
-        initList.append(random.randint(0, pins))
-
-    return initList
+    return np.random.randint(0, pins, lines, dtype='int64')
 
 #print(initialization(4000, 256))
