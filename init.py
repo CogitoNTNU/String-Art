704ccf887b41a84c3d6f13ee90a5d8b046675fe6
Jon Rodtang <jonrodtang@gmail.com>
2021-10-12 18:53:14 +0200

    Removed comments

--- init.py
+++ init.py
@@ -1,19 +1,9 @@
 import numpy as np
 import random
-#define number of lines
-#define number of pins
-#generate list of length lines with values 0 to pins-1
-#return initialized list
 #import sys
 #np.set_printoptions(threshold=sys.maxsize)
-'''
-def initialize_single_indiuvidual(lines: int, pins: int):
-    # N lines result in N+1 elements
-    return np.random.randint(0, pins, size=lines+1)
-'''
-
-
 def initialize_population(num_induviduals: int, lines: int, pins: int):
+    # N lines result in N+1 elements
     # Generate 2d numpy array with of containg induviduals of length lines+1
     population = np.random.randint(0, pins, size=(num_induviduals,lines+1))
     # Check if two consecutive elements are equal: if so generate new element till it is false
