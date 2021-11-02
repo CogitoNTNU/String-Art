73c5aab9b927b36df3bdff8ed497cf77bf4ad083
Jon Rodtang <jonrodtang@gmail.com>
2021-11-02 20:07:59 +0100

    Selection algorithm cleaned file

--- selection.py
+++ selection.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+'''
 pins = 256
 num_induviduals = 4
 lines = 12
@@ -7,12 +8,14 @@ population1 = np.random.randint(0, pins, size=(num_induviduals,lines+1))
 population2 = np.random.randint(0, pins, size=(num_induviduals,lines+1))
 population1_fit = np.random.rand(1,num_induviduals)
 population2_fit = np.random.rand(1,num_induviduals)
+'''
 
-def selection(population_one, population_two, population_one_fitness, population_two_fitness,):
+def selection(population_one, population_two, population_one_fitness, population_two_fitness):
 
     top_individuals = int(population_one.shape[0]/2)
     random_bottom_sample = int(population_one.shape[0]/2)
 
+    # Combine parent and child generations
     population_start = np.concatenate((population_one, population_two))
     population_start_fitness = np.concatenate((population_one_fitness, population_two_fitness), axis=None)
 
@@ -38,6 +41,3 @@ def selection(population_one, population_two, population_one_fitness, population
     fitness_final = np.concatenate((fitness_top, fitness_bottom_sample))
     population_final = np.concatenate((population_top, bottom_sample))
     return population_final, fitness_final
-
-
-print(selection(population1, population2, population1_fit, population2_fit))
